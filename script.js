f6efd2f36d2cecbf907cca683ad40d870651ffd5
SeanOVeryMac <seanovery1@gmail.com>
2021-09-01 14:28:03 -0400

    added arrays for possible password characters

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters = lowerLetters.map(letter => letter.toUpperCase());
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!", "#", "$", "%", "&", "@", "*"];
+console.log(lowerLetters);
+console.log(upperLetters);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
