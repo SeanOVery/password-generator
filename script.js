500a324dc419b5e207acec0f72ce4c24b1c67e59
SeanOVeryMac <seanovery1@gmail.com>
2021-09-06 10:19:43 -0400

    added prompts and variables to get user selections for aspects of password

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var upperLetters = lowerLetters.map(letter => letter.toUpperCase());
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = ["!", "#", "$", "%", "&", "@", "*"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  window.confirm("Please answer these questions to we can create a password for you!");
+  let passLowerCase = window.prompt("Would you like lower case letters in your password?", "Please type Yes or No");
+  let passUpperCase = window.prompt("Would you like upper case letters in your password?", "Please type Yes or No");
+  let passNumbers = window.prompt("Would you like numbers in your password?", "Please type Yes or No");
+  let passSpecialChars = window.prompt("Would you like special characters(#, $, etc) in your password?", "Please type Yes or No");
+  let passLength = window.prompt("Please set the length of the password you would like between 8-128 characters", "Please type a number between 8 and 128");
+  
+  function generatePassword() {
 
-  passwordText.value = password;
+    }
+  
+    passwordText.value = password;
+  
 
 }
 
