ef98eb253235da3462765a999da3f764de5f11e6
SeanOVeryMac <seanovery1@gmail.com>
2021-09-08 17:13:59 -0400

    better variable declaration and input converted to a function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var generateBtn = document.querySelector("#generate"),
+let generateBtn = document.querySelector("#generate"),
     lowerLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     upperLettersArray = lowerLettersArray.map(letter => letter.toUpperCase()),
     numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
@@ -7,42 +7,43 @@ var generateBtn = document.querySelector("#generate"),
 // variable and array setup
 
 function writePassword() {
-  var password = '',
+  let password = '',
+      passLowerCase = '',
+      passUpperCase = '',
+      passNumbers = '',
+      passSpecialChars = '',
+      passLength = '',
       passwordText = document.querySelector("#password");
 
-  window.alert("Please answer these questions so we can create a password for you! You must select at least 1 type of character to use in the password.");
-
-  var passLowerCase = window.prompt("Would you like lower case letters in your password?", "Please type Yes or No").toLowerCase();
+  function userInput() {
+    window.alert("Please answer these questions so we can create a password for you! You must select at least 1 type of character to use in the password.");
+    passLowerCase = window.prompt("Would you like lower case letters in your password?", "Please type Yes or No").toLowerCase();
     if ((passLowerCase !== "yes") && (passLowerCase !== "no")) {
       console.log(passLowerCase)
       return window.alert("Please restart and enter Yes or No in the prompt!");
     }
-
-  var passUpperCase = window.prompt("Would you like upper case letters in your password?", "Please type Yes or No").toLowerCase();
+    passUpperCase = window.prompt("Would you like upper case letters in your password?", "Please type Yes or No").toLowerCase();
     if ((passUpperCase !== "yes") && (passUpperCase !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     }
-
-  var passNumbers = window.prompt("Would you like numbers in your password?", "Please type Yes or No").toLowerCase();
+    passNumbers = window.prompt("Would you like numbers in your password?", "Please type Yes or No").toLowerCase();
     if ((passNumbers !== "yes") && (passNumbers !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     }
-  
-  var passSpecialChars = window.prompt("Would you like special characters(#, $, etc) in your password?", "Please type Yes or No").toLowerCase();
+    passSpecialChars = window.prompt("Would you like special characters(#, $, etc) in your password?", "Please type Yes or No").toLowerCase();
     if ((passLowerCase === "no") && (passUpperCase === "no") && (passNumbers === "no") &&  (passSpecialChars === "no")) {
       return window.alert("Please select at least 1 type of character to use in the password!");
     } else if ((passSpecialChars !== "yes") && (passSpecialChars !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     } 
-
-  var passLength = window.prompt("Please set the length of the password you would like between 8-128 characters", "Please type a number between 8 and 128");
+    passLength = window.prompt("Please set the length of the password you would like between 8-128 characters", "Please type a number between 8 and 128");
     if ((passLength < 8 || passLength > 128) && typeof passLength === "number"){
       return window.alert("Please Restart and enter a number between 8 and 128!");
     }
-    // alerts and prompts dealing with the user input that sets up the password criteria including error handling of improper inputs
-
+  }// function using alerts and prompts to take the user input that sets up the password criteria including error handling of improper inputs
+  userInput();
+  
   function charArrayFunc() {
-    
     if (passLowerCase === "yes" && passUpperCase === "no" && passNumbers === "no" && passSpecialChars === "no") {
       return charArray = lowerLettersArray;
     } else if (passLowerCase === "no" && passUpperCase === "yes" && passNumbers === "no" && passSpecialChars === "no") {
@@ -85,7 +86,7 @@ function writePassword() {
   charArrayFunc();
 
   function generatePassword() {
-    var charArrayJoined = charArray.join("");
+    let charArrayJoined = charArray.join("");
     for (var i = 1; i <= passLength; i++) {
       password += charArrayJoined.charAt(Math.floor(Math.random()* charArray.length));
     }
