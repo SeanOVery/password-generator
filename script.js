8d6c44aa78b57ef5534c212a7c2c7d37a98fef0d
SeanOVeryMac <seanovery1@gmail.com>
2021-09-08 17:38:29 -0400

    converted some variables to const

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function writePassword() {
       passSpecialChars = '',
       passLength = '',
       passwordText = document.querySelector("#password"),
-      lowerLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-      upperLettersArray = lowerLettersArray.map(letter => letter.toUpperCase()),
-      numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-      specialCharsArray = ["!", "#", "$", "%", "&", "@", "*"],
       charArray = [];
+  const lowerLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+        upperLettersArray = lowerLettersArray.map(letter => letter.toUpperCase()),
+        numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        specialCharsArray = ["!", "#", "$", "%", "&", "@", "*"];
+      
   // local variable setup for writePassword
   
   function userInput() {
