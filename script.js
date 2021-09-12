94dd0a25ece2a80917da13afb13903348bbceb10
SeanOVeryMac <seanovery1@gmail.com>
2021-09-12 08:34:47 -0400

    finished alternative charArrayFunc function

--- script.js
+++ script.js
@@ -14,16 +14,12 @@ function writePassword() {
         numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
         specialCharsArray = ["!", "#", "$", "%", "&", "@", "*"];
   const inputObject = {
-        passLowerCaseKey: false,
-        passUpperCaseKey: false,
-        passNumbersKey: false,
-        passSpecialCharsKey: false
+        passLowerCaseKey: '',
+        passUpperCaseKey: '',
+        passNumbersKey: '',
+        passSpecialCharsKey: ''
   }
-  
-  
-  
-  
-        // local variable setup for writePassword
+        // local variable and object setup for writePassword
   
   function userInput() {
     window.alert("Please answer these questions so we can create a password for you! You must select at least 1 type of character to use in the password.");
@@ -37,11 +33,10 @@ function writePassword() {
     if ((passLowerCase !== "yes") && (passLowerCase !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     } else if (passLowerCase === "yes") {
-      passLowerCaseKey = true;
+      inputObject.passLowerCaseKey = true;
     } else {
-      passLowerCaseKey = false;
+      inputObject.passLowerCaseKey = false;
     }
-    
     
     passUpperCase = window.prompt("Would you like upper case letters in your password?", "Please type Yes or No");
     if (passUpperCase === null) {
@@ -52,9 +47,9 @@ function writePassword() {
     if ((passUpperCase !== "yes") && (passUpperCase !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     } else if (passUpperCase === "yes") {
-      passUpperCaseKey = true;
+      inputObject.passUpperCaseKey = true;
     } else {
-      passUpperCaseKey = false;
+      inputObject.passUpperCaseKey = false;
     }
     
     passNumbers = window.prompt("Would you like numbers in your password?", "Please type Yes or No");
@@ -66,9 +61,9 @@ function writePassword() {
     if ((passNumbers !== "yes") && (passNumbers !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     } else if (passNumbers === "yes") {
-      passNumbersKey = true;
+      inputObject.passNumbersKey = true;
     } else {
-      passNumbersKey = false;
+      inputObject.passNumbersKey = false;
     }
     
     passSpecialChars = window.prompt("Would you like special characters(#, $, etc) in your password?", "Please type Yes or No");
@@ -82,9 +77,9 @@ function writePassword() {
     } else if ((passSpecialChars !== "yes") && (passSpecialChars !== "no")) {
       return window.alert("Please restart and enter Yes or No in the prompt!");
     } else if (passSpecialChars === "yes") {
-      passSpecialCharsKey = true;
+      inputObject.passSpecialCharsKey = true;
     } else {
-      passSpecialCharsKey = false;
+      inputObject.passSpecialCharsKey = false;
     }
     
     passLength = window.prompt("Please set the length of the password you would like between 8-128 characters", "Please type a number between 8 and 128");
@@ -103,44 +98,11 @@ function writePassword() {
   userInput();
   
   function charArrayFunc() {
-    if (passLowerCase === "yes" && passUpperCase === "no" && passNumbers === "no" && passSpecialChars === "no") {
-      return charArray = lowerLettersArray;
-    } else if (passLowerCase === "no" && passUpperCase === "yes" && passNumbers === "no" && passSpecialChars === "no") {
-      return charArray = upperLettersArray;
-    } else if (passLowerCase === "no" && passUpperCase === "no" && passNumbers === "yes" && passSpecialChars === "no") {
-      return charArray = numbersArray;
-    } else if (passLowerCase === "no" && passUpperCase === "no" && passNumbers === "no" && passSpecialChars === "yes") {
-      return charArray = specialCharsArray;
-    } // handles cases where user selects 1 character type
-      
-      else if (passLowerCase === "yes" && passUpperCase === "yes" && passNumbers === "no" && passSpecialChars === "no") {
-      return charArray = lowerLettersArray.concat(upperLettersArray);
-    } else if (passLowerCase === "yes" && passUpperCase === "no" && passNumbers === "yes" && passSpecialChars === "no") {
-      return charArray = lowerLettersArray.concat(numbersArray);
-    } else if (passLowerCase === "yes" && passUpperCase === "no" && passNumbers === "no" && passSpecialChars === "yes") {
-      return charArray = lowerLettersArray.concat(specialCharsArray);
-    } else if (passLowerCase === "no" && passUpperCase === "yes" && passNumbers === "yes" && passSpecialChars === "no") {
-      return charArray = upperLettersArray.concat(numbersArray);
-    } else if (passLowerCase === "no" && passUpperCase === "yes" && passNumbers === "no" && passSpecialChars === "yes") {
-      return charArray = upperLettersArray.concat(specialCharsArray);
-    } else if (passLowerCase === "no" && passUpperCase === "no" && passNumbers === "yes" && passSpecialChars === "yes") {
-      return charArray = numbersArray.concat(specialCharsArray);
-    } // handles cases where user selects 2 character types
-      
-      else if (passLowerCase === "yes" && passUpperCase === "yes" && passNumbers === "yes" && passSpecialChars === "no") {
-      return charArray = lowerLettersArray.concat(upperLettersArray, numbersArray);
-    } else if (passLowerCase === "yes" && passUpperCase === "yes" && passNumbers === "no" && passSpecialChars === "yes") {
-      return charArray = lowerLettersArray.concat(upperLettersArray, specialCharsArray);
-    } else if (passLowerCase === "yes" && passUpperCase === "no" && passNumbers === "yes" && passSpecialChars === "yes") {
-      return charArray = lowerLettersArray.concat(numbersArray, specialCharsArray);
-    } else if (passLowerCase === "no" && passUpperCase === "yes" && passNumbers === "yes" && passSpecialChars === "yes") {
-      return charArray = upperLettersArray.concat(numbersArray, specialCharsArray);
-    } // handles cases where user selects 3 character types
-      
-      else {
-      return charArray = lowerLettersArray.concat(upperLettersArray, numbersArray, specialCharsArray);
-    }  // handles final case where user selects all 4 character types.
-  } // funciton to create an array containing all characters the user chooses
+    if (inputObject.passLowerCaseKey) {charArray = charArray.concat(lowerLettersArray);} 
+    if (inputObject.passUpperCaseKey) {charArray = charArray.concat(upperLettersArray);}
+    if (inputObject.passNumbersKey) {charArray = charArray.concat(numbersArray);}
+    if (inputObject.passSpecialCharsKey) {charArray = charArray.concat(specialCharsArray);}
+  } // function to create an array containing all characters the user chooses
   charArrayFunc();
 
   function generatePassword() {
